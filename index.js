dd4666e4990542538dda9a0d4b83e5f511aa4f9f
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2025-01-09 21:17:38 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const app = express(); 
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+    res.status(200).json({prenom:'anna'})
+})
+//app.post qui recoit n objet une requete nom et prenom et age et qui retourne si l'utilisateur est apte a rentrer ou pas 
+let users = [];
+app.post('/users', (req,res) => {
+    console.log(req);
+    
+    const {firstName, lastName, age} = req.body;
+    if(!firstName || !lastName || !age === undefined){
+        return res.status(400).json({message: 'Tous les champs sont obligatoires (firstName, lastName, age).'});
+    }
+    if(age< 18){
+return res.status(400).json({message: 'l utilisateur doit au moins avoir 18 ans pour creer un compte'});
+    }
+    else{
+        return res.status(200).json({firstName, lastName, age});
+    }
+});
+
+
+
+app.listen(3000,()=>{
+    console.log("executer dans le port 3000");
+    
+});
