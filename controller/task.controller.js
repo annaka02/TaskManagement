7ffb41e9490f59814ea9e5f1f371f96b6d59863c
Anna Ka <anna.ka@uvs.edu.sn>
2025-01-10 21:04:38 +0000

    exercices

---
+++ controller/task.controller.js
@@ -0,0 +1,9 @@
+//creer 5controller pour lister
+//  toutes les taches 
+// une seule tache 
+//ajouter des taches 
+//supprimer des taches
+//modifier une tache 
+// pouvoir acceder aux taches depuis index.js 
+// git add . git commit -m "commentaire" git push 
+// npm init npm install express / body-parser
