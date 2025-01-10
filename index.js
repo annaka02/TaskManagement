a2d1fca9862cbd70383708cbf1f7cbb95e7f0f04
Anna Ka <anna.ka@uvs.edu.sn>
2025-01-10 20:08:11 +0000

    crud user

--- index.js
+++ index.js
@@ -11,24 +11,55 @@ app.get('/',(req,res)=>{
 app.post('/users', (req,res) => {
     console.log(req);
     
-    const {firstName, lastName, age} = req.body;
-    if(!firstName || !lastName || !age === undefined){
-        return res.status(400).json({message: 'Tous les champs sont obligatoires (firstName, lastName, age).'});
+    const {id, firstName, lastName, age} = req.body;
+    if(!id || !firstName || !lastName || age === undefined){
+        return res.status(400).json({message: 'Tous les champs sont obligatoires (id, firstName, lastName, age).'});
     }
     if(age< 18){
-return res.status(400).json({message: 'l utilisateur doit au moins avoir 18 ans pour creer un compte'});
+       return res.status(400).json({message: 'l utilisateur doit au moins avoir 18 ans pour creer un compte'});
     }
     else{
-        return res.status(200).json({firstName, lastName, age});
+        return res.status(200).json({id,firstName, lastName, age});
     }
 });
-let users = [];
+
 app.post('/addUsers', (req,res) => {
     users.push(req.body);
     return res.status(200).json(users);
 })
-// delete user and update user
+let users = [
+    { id: 1, firstName: "Anna", lastName: "Ka", age: 25 },
+    { id: 2, firstName: "Ali", lastName: "Ndiaye", age: 30 }
+];
 
+// delete user 
+app.delete('/deleteUser/:id', (req, res) => {
+    const id = req.params.id;
+    const index = users.findIndex(user => user.id == id);
+    if(index === -1){
+        return res.status(404).json({message: 'Utilisateur non trouvé'});
+    }
+    res.status(200).json({
+        message: `Utilisateur avec ID ${id} supprimé avec succès.`,
+    remainingUsers: users
+});
+});
+
+
+
+//update 
+app.put('/updateUser/:id', (req, res) => {
+    const id = req.params.id;
+    const index = users.findIndex(user => user.id == id);
+    if(index === -1){
+        return res.status(404).json({message: 'Utilisateur non trouvé'});
+    }
+    users[index] = req.body;
+    res.status(200).json({
+        message: `Utilisateur avec ID ${id} modifié avec succès.`,
+    updatedUser: users[index]
+});
+});
 
 app.listen(3000,()=>{
     console.log("executer dans le port 3000");
